a47ca369431486445759a1f9d4c4cfbae8313927
NelsonLeguizamo <>
2025-04-15 10:47:00 -0300

    trabajito

---
+++ TP 3/10.py
@@ -0,0 +1,7 @@
+x1=float(input("ingrese la cordenada x del punto 1"))
+x2=float(input("ingrese la cordenada x del punto 2"))
+y1=float(input("ingrese la cordenada y del punto 1"))
+y2=float(input("ingrese la cordenada y del punto 2"))
+
+distancia=((x1-x2)**2+(y1-y2)**2)**0.5
+print("La distancia es: ",distancia)
---
+++ TP 3/4.py
@@ -0,0 +1,4 @@
+tiempo = float(input("ingrese el tiempo en segundos: "))
+distancia = float(input("ingrese la distancia en metros: "))
+v = distancia/tiempo
+print("La velocidad es: ", v, "m/s")
