73fcdefce021ed59b8e20a9f834e1285a61f4c5e
ThiagoLoBalbo <204006247+luci0qsy@users.noreply.github.com>
2025-05-27 09:24:24 -0300

    cositas

--- Ejercicio 1.py
+++
@@ -1,2 +0,0 @@
-for i in range(5):
-    nota=int(input(f"Nota del alumno {i}:"))
---
+++ Tp 2/ejercicio 2.py
@@ -0,0 +1,18 @@
+niño = 0
+ad = 0
+adult = 0
+
+while True:
+  edad = int(input("ingresa una edad (0 para terminar): "))
+              
+  if edad == 0:
+      break
+  elif edad <13:
+      niño += 1
+  elif 13 <= edad <= 17:
+      ad <= 1
+  else:
+      adult += 1
+print("Cantidad de niños", niño )
+print("Cantidad de adolecentes", ad)
+print("Cantidad de adultos", adult)
